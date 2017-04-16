f4a904ab69e1eca8a0a7bf7cc92740f501431379
AnuragParihar <anuragparihar.apy@gmail.com>
2017-04-17 00:47:01 +0530

    Final Submit
    s

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,10 +1,13 @@
 cubicleApp.controller('homeController', function($scope, $rootScope, $http, $location, $uibModal, $log, $document, $window) {
 	
+	$scope.showTabs=false;
+	
+	// Ajax Call failed due to CORS
 	$scope.searchArtist = function() {
 		if($scope.artistName=="Jack"&&$scope.limit=="4"){
 			$http({
 			method : 'GET',
-			url : host.concat("search?term=", $scope.artistName, "&limit=", $scope.limit),
+			url : host.concat("search?term=", $scope.artistName, "&limit=", $scope.limit, "&callback=dataReceiver"),
 			headers : {
 				'Content-Type' : 'application/json'
 			}
@@ -24,19 +27,17 @@ cubicleApp.controller('homeController', function($scope, $rootScope, $http, $loc
 			alert("Please Enter Artist Name as Jack and Limit as 4");
 		}
 	};
-	$scope.showTabs=false;
+	
+	// Ajax Call failed due to CORS
 	$scope.initJack = function(){
 		$http({
 			method : 'GET',
-			url : host.concat("search?term=Jack&limit=1"),
+			url : host.concat("search?term=Jack&limit=1", "&callback=dataReceiver"),
 			headers : {
 				'Content-Type' : 'application/json'
 			}
 		}).then(function(response) {
 			$scope.status = response.status;
-			$scope.artistNameFetched = response.data.results.artistName;
-			$scope.trackNameFetched = response.data.results.trackName;
-			$scope.artworkFetched = response.data.results.artworkUrl30;
 			console.log("Webservice Get Success ");
 			$("#searchModal").modal('hide');
 		}, function(response) {
@@ -44,18 +45,35 @@ cubicleApp.controller('homeController', function($scope, $rootScope, $http, $loc
 		});
 	};
 	$scope.initJack();
+	$scope.artistNameFetched = window.data.results[0].artistName;
+	$scope.trackNameFetched = window.data.results[0].trackName;
+	$scope.artworkFetched = window.data.results[0].artworkUrl30;
+	
+	$scope.artistNameFetched1 = window.data.results[1].artistName;
+	$scope.trackNameFetched1 = window.data.results[1].trackName;
+	$scope.artworkFetched1 = window.data.results[1].artworkUrl30;
+	
+	$scope.artistNameFetched2 = window.data.results[2].artistName;
+	$scope.trackNameFetched2 = window.data.results[2].trackName;
+	$scope.artworkFetched2 = window.data.results[2].artworkUrl30;
+	
+	$scope.artistNameFetched3 = window.data.results[3].artistName;
+	$scope.trackNameFetched3 = window.data.results[3].trackName;
+	$scope.artworkFetched3 = window.data.results[3].artworkUrl30;
+			
+	// Ajax Call failed due to CORS
 	$scope.callJack = function(limit){
 		$http({
 			method : 'GET',
-			url : host.concat("search?term=Jack&limit=",limit),
+			url : host.concat("search?term=Jack&limit=",limit, "&callback=dataReceiver"),
 			headers : {
 				'Content-Type' : 'application/json'
 			}
 		}).then(function(response) {
 			$scope.status = response.status;
-			$scope.artistNameFetched = response.data.results.artistName;
-			$scope.trackNameFetched = response.data.results.trackName;
-			$scope.artworkFetched = response.data.results.artworkUrl30;
+			$scope.artistNameFetched = window.data[0].results.artistName;
+			$scope.trackNameFetched = window.data[0].results.trackName;
+			$scope.artworkFetched = window.data[0].results.artworkUrl30;
 			console.log("Webservice Get Success ");
 			$("#searchModal").modal('hide');
 		}, function(response) {
